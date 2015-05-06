b517e99aaa05e106ec2e9a50a461d1a39225a9df
Volodymyr_Partyka <vovuk097@gmail.com>
2015-05-06 13:48:06 +0300

    update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in(@user)
+      #sign_in(@user)
       flash[:success] = 'Welcome !'
       redirect_to @user, notice: 'You signed up successfully'
     else
